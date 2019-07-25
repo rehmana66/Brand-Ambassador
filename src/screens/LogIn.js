29daabeb5d048a314dfb1df2c84be49d93fb38fe
Colin Keys <colin.j.keys@gmail.com>
2019-07-24 18:30:28 -0600

    New Log In format and text inputs

--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -1,98 +1,70 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
+import { Dimensions, View, Text, StyleSheet, Image, Button, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import  Reinput  from 'reinput';
 
 class LogIn extends Component {
 
     render() {
         return (
-            <View style = {styles.mainContainer}>
-                <View style = {styles.innerContainer}>
+            <SafeAreaView forceInset = {{ bottom: 'always' }} style = {{ flex: 1, backgroundColor: '#dff3fd' }} onPress ={ () => {
+                Keyboard.dismiss() }}>
+                <View style = { styles.loginContainer} >
                     <View style = {{alignSelf: 'center', paddingTop: 10}}>
                         <Image
                         style={{width: 80, height: 80}}
                         source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}/>
                     </View>
                     <Text style = {styles.loginText}>Log In</Text>
-                    <TextInput  style = {styles.input}
-                        autoCorrect = {false}
-                        underlineColorAndroid = "rgba(0, 0, 0, 0)"
-                        placeholder = "Email Address"
-                        placeholderTextColor = "#6c9192"/>
-                    <TextInput  style = {styles.input}
-                        secureTextEntry = {true}
-                        underlineColorAndroid = "transparent"
-                        placeholder = "Password"
-                        placeholderTextColor = "#6c9192"/>
-                    <TouchableOpacity>
-                        <View style = {styles.forgotPassButton}>
-                            <Text style = {styles.forgotPassButtonText}>Forgot Password?</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-                <View style = {styles.loginButtonContainer}>
-                    <View style = {{margin: 10}}>
-                        <Button style = {styles.loginButton}
-                            title = "Log In"
-                            color = '#045b95'
-                            borderColor = '#dff3fd'
-                            onPress = {() => this.props.navigation.navigate('Dashboard')}/>
+                    <Reinput label = "Email Address" />
+                    <Reinput label = "Password"  secureTextEntry = {true}/>
+                    <View style = {{flexDirection: 'row', justifyContent: 'center'}}>
+                        <TouchableOpacity>
+                            <View style = {styles.loginScreenButtons}>
+                                <Text style = {styles.loginScreenButtonsText}>Forgot Password?</Text>
+                            </View>
+                        </TouchableOpacity>
+                        <TouchableOpacity onPress = {() => this.props.navigation.navigate('Dashboard')}>
+                            <View style = {styles.loginScreenButtons}>
+                                <Text style = {styles.loginScreenButtonsText}>Log In</Text>
+                            </View>
+                        </TouchableOpacity>
                     </View>
                 </View>
-            </View>
+            </SafeAreaView>
         );
     }
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
-        flex: 1
+        flex: 1,
+        backgroundColor: '#dff3fd'
     },
     loginText: {
-        margin: 10,
+        paddingBottom: 10,
         fontSize: 26,
-        color: '#ffffff'
+        color: 'black'
     },
-    input: {
-        backgroundColor: '#03396a',
-        margin: 5,
-        height: 50,
-        borderColor: '#dff3fd',
-        borderWidth: 1,
+    loginScreenButtons: {
+        backgroundColor: '#dff3fd',
         borderRadius: 10,
-        color: 'white',
-        fontSize: 16
-    },
-    loginButton: {
-       borderColor: '#dff3fd',
-       borderRadius: 10,
-       borderWidth: 1,
-       padding: 10,
-       margin: 10,
-       height: 40,
-    },
-    submitButtonText:{
-       color: 'white'
-    },
-    forgotPassButton: {
-        backgroundColor: '#021f4b',
-        borderRadius: 10,
+        width: Dimensions.get('window').width/2 - 30,
         margin: 5,
         height: 50
     },
-    forgotPassButtonText: {
-        color: 'white',
+    loginScreenButtonsText: {
+        color: '#3f51b5',
         padding: 14,
-        fontSize: 16
+        fontSize: 16,
+        alignSelf: 'center'
     },
-    loginButtonContainer: {
-        height: 55,
-        backgroundColor: '#1a171c', 
-        justifyContent: 'flex-end'
+    loginContainer: {
+        backgroundColor: '#dff3fd',
+        marginHorizontal: 15,
+        marginVertical: 20,
+        width : Dimensions.get('window').width - 30,
     },
-    innerContainer: {
-        flex: 5, 
-        backgroundColor: '#021f4b'
-    }
  })
 
 export default LogIn;
